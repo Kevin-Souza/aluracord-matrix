0ee5d1faff0da1312a0bb0b75cc30b8070d57dc8
Kevin-Souza <kevinsouza1.ks@gmail.com>
2022-01-25 20:53:17 -0300

    configs globais no _app.js

--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,6 @@ import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-//componente para estilos globais da aplicação
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
-
 // Componente React
 function Titulo(props) {
     const Tag = props.tag || 'h1' ;
@@ -73,7 +43,6 @@ export default function PaginaInicial() {
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
